ee3be6c6792b7ef8d7129fc78eb591a735b02b93
794963 <794963@seq.org>
2020-02-04 10:25:00 -0800

    fgn

--- Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
+++ Elevens/Activity Starter Code/Activity2  Starter Code/DeckTester.java
@@ -12,6 +12,14 @@ public class DeckTester {
         String[] suits = {"Square","Circle"};
         int[] pV ={1,2,3};
         Deck d1 = new Deck(ranks,suits,pV);
+        String[] ranks2 ={"D","E","F"};
+        String[] suits2 = {"S","C"};
+        int[] pV2 ={4,5,6};
+        Deck d2 = new Deck(ranks2,suits2,pV2);
+        String[] ranks3 ={"G","H","I"};
+        String[] suits3 = {"B","A"};
+        int[] pV3 ={7,8,9};
+        Deck d3 = new Deck(ranks3,suits3,pV3);
         
     }
 }
---
+++ Shuffle/Shuffle.java
@@ -0,0 +1,43 @@
+
+/**
+ * Shuffles
+ *
+ * @author (Jaan Sigel)
+ * @version (2/4/20)
+ */
+import java.util.*;
+public class Shuffle
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class Shuffle
+     */
+    public Shuffle(int[] s)
+    {
+        // initialise instance variables
+        int rand = 0;
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        ArrayList<Integer> fin = new ArrayList<Integer>();
+        for( int i=0; i<52;i++){
+        temp.add(s[i]);
+    }
+    System.out.println(temp);
+    for(int i=0; i<52;i++){
+        rand =(int)(Math.random()*temp.size());
+        fin.add(temp.get(rand));
+        temp.remove(rand);
+    }
+    System.out.println(fin);
+    }
+
+    public static void main(){
+    int[] nums =  new int[52];
+    for( int i=0; i<52;i++){
+        nums[i]=i+1;
+        
+    }
+    Shuffle s = new Shuffle(nums);
+    }
+}
