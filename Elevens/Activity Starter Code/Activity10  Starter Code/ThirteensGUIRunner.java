56bb639373e5dcd3d627f8928609025b17b7df7c
794963 <794963@seq.org>
2020-02-24 10:23:31 -0800

    1

---
+++ Elevens/Activity Starter Code/Activity10  Starter Code/ThirteensGUIRunner.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class ThirteensGUIRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ThirteensGUIRunner
+{
+    
+	/**
+	 * Plays the GUI version of Elevens.
+	 * @param args is not used.
+	 */
+	public static void main(String[] args) {
+		Board board = new ThirteensBoard();
+		CardGameGUI gui = new CardGameGUI(board);
+		gui.displayGame();
+	}
+}
